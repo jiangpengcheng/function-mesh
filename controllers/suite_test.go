343379ed30386d59d5d89e018cd8c4a27246c888
jiangpengcheng <scjiangpengcheng@gmail.com>
2024-04-26 14:14:34 +0800

    Fix bump error

--- controllers/suite_test.go
+++ controllers/suite_test.go
@@ -35,6 +35,7 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/envtest"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+	"sigs.k8s.io/controller-runtime/pkg/metrics/server"
 	// +kubebuilder:scaffold:imports
 )
 
@@ -80,8 +81,10 @@ var _ = BeforeSuite(func(done Done) {
 	// +kubebuilder:scaffold:scheme
 
 	k8sManager, err := ctrl.NewManager(cfg, ctrl.Options{
-		Scheme:             scheme.Scheme,
-		MetricsBindAddress: ":8090",
+		Scheme: scheme.Scheme,
+		Metrics: server.Options{
+			BindAddress: ":8090",
+		},
 	})
 	Expect(err).ToNot(HaveOccurred())
 
